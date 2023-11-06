4e83d9472260ab850bab8809ea3090636a4db0b7
José Wagner Dutra Porto <jwagnerdporto@gmail.com>
2023-11-06 20:27:49 -0300

    Exercicios Basicos

---
+++ JavaScript/exercicios_basicos/1_exercicio/script.js
@@ -0,0 +1,3 @@
+console.log("Hello World");
+console.log('Meu nome é Wagner');
+console.log(`Este aqui é o template literal`);
---
+++ JavaScript/exercicios_basicos/4_exercicio/script.js
@@ -0,0 +1,3 @@
+console.log(5 < 10 && 10 != 5);
+console.log(5 < 10 || 10 != 5);
+console.log(!(5 < 10 && 10 != 5));
