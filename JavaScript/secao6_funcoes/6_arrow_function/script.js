2a1eb0d5da7870bba2ca4f62998462a92f8ab6a6
José Wagner Dutra Porto <jwagnerdporto@gmail.com>
2023-11-29 20:23:05 -0300

    Aula JavaScript 29-11

---
+++ JavaScript/secao6_funcoes/6_arrow_function/script.js
@@ -0,0 +1,13 @@
+let consoleTeste = () => {
+    console.log("Olá mundo!");
+}
+
+consoleTeste();
+
+let soma = (a,b) => {
+    return a + b;
+};
+
+soma(10,20);
+
+console.log(soma(10,20));
---
+++ JavaScript/secao6_funcoes/7_arrow_function_2/script.js
@@ -0,0 +1,9 @@
+const  multiplicadorPorDois = (x) => {
+    return x * 2;
+}
+
+console.log(multiplicadorPorDois(2));
+
+const multiplicadorPorDois2 = x => x * 2;
+
+console.log(multiplicadorPorDois2(2));
